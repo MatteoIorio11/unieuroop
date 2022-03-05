76d3b2638bb9f1f51c71e9006b81d31b469587d9
Samuele Ferri <samuele1301@gmail.com>
2022-03-05 16:05:56 +0100

    implementation of the costructor in AbstractPersonImpl

---
+++ app/src/main/java/unieuroop/model/person/AbstractPersonImpl.java
@@ -0,0 +1,9 @@
+package unieuroop.model.person;
+
+import java.sql.Date;
+
+public abstract class AbstractPersonImpl {
+	private String name;
+	private String surname;
+	private final Date birthdayDate;
+}
