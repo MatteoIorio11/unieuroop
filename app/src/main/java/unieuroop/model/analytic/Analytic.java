55988a81d1083a45d6c09a8ce1de213ef333a180
Matteo <matteo.iorio01@gmail.com>
2022-03-05 10:24:53 +0100

    Implementation of the class Sale : construction of the Constructor

--- app/src/main/java/unieuroop/model/analytic/Analytic.java
+++ app/src/main/java/unieuroop/model/analytic/Analytic.java
@@ -1,5 +1,20 @@
 package unieuroop.model.analytic;
 
+import java.util.List;
+import java.util.LinkedList;
+import unieuroop.model.sale.Sale;
+
+/**
+ * 
+ * Class used for Analytics on sales
+ *
+ */
 public class Analytic {
 	
+	private final List<Sale> sales;
+	
+	
+	public Analytic() {
+		this.sales = new LinkedList<>();
+	}
 }
