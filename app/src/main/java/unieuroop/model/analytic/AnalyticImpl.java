eb55be8b007c873d2b44ef08aaeafa816d1a5977
Matteo <matteo.iorio01@gmail.com>
2022-03-05 12:05:12 +0100

    creation of method getQuantitySoldOf in Analytic

--- app/src/main/java/unieuroop/model/analytic/AnalyticImpl.java
+++ app/src/main/java/unieuroop/model/analytic/AnalyticImpl.java
@@ -1,11 +1,15 @@
 package unieuroop.model.analytic;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.LinkedList;
 import unieuroop.model.sale.Sale;
 import unieuroop.model.product.Product;
+import unieuroop.model.product.Category;
 import unieuroop.model.sale.NullSaleException;
 
 public class AnalyticImpl {
@@ -38,5 +42,20 @@ public class AnalyticImpl {
 	                .flatMap((sale) -> sale.getProducts().stream())
 	                .distinct()
 	                .collect(Collectors.toList());
+	}
+	public int getQuantitySoldOf(final Product product) {
+	    return 0;
+	}
+	/**
+	 * PROMEMRORIA : NELLA VIEW DOVRO COSTRUIRE UN SET DELLE CATEOGRIE E NEL PREDICATE METTERE
+	 * (categoria) -> set.contains(categoria) ====> DOVE set VIENE COSTRUITO IN BASE A CIO CHE SI SCEGLIE DALLA VIEW 
+	 * @return a
+	 */
+	public Map<Product, Integer> getOrdered(final Predicate<Set<Category>> predicate) {
+	    final Map<Product, Integer> out = this.sales.stream()
+	            .flatMap((sale) -> sale.getProducts().stream()
+	                    .filter((product) -> predicate.test(product.getCategory())))
+	            .collect(Collectors.toMap((product) -> product, (prod)))
+	    return null;
 	}
 }
