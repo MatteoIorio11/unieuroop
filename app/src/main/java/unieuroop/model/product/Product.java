7f195274994ec08d310074dafc85758324a092d1
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-03-05 11:42:28 +0100

    product interface and productImpl

--- app/src/main/java/unieuroop/model/product/Product.java
+++ app/src/main/java/unieuroop/model/product/Product.java
@@ -1,15 +1,59 @@
 package unieuroop.model.product;
 
 public interface Product {
+    /**
+     * 
+     * @return : the unique code of the product
+     */
+    int getProductCode();
+    /**
+     * 
+     * @return : the name of the product
+     */
     String getName();
-
+    /**
+     * 
+     * @return : the price the shop decided as selling price
+     */
     float getSellingPrice();
-
+    /**
+     * 
+     * @return : the price payed by the shop to get this product
+     */
     float getPurchasePrice();
-
+    /**
+     * 
+     * @return : a general description of the product
+     */
     String getDescription();
-
+    /**
+     * 
+     * @return : the category of the product
+     */
     Category getCategory();
-
+    /**
+     * 
+     * @return : the supplier that sold the product to the shop
+     */
     Supplier getSupplier();
+    /**
+     * change the name of the product.
+     * @param name
+     */
+    void setName(String name);
+    /**
+     * change the selling price of the product.
+     * @param price
+     */
+    void setSellingPrice(float price);
+    /**
+     * change the description of the product.
+     * @param description
+     */
+    void setDescription(String description);
+    /**
+     * change the category of the product.
+     * @param category
+     */
+    void setCategory(Category category);
 }
