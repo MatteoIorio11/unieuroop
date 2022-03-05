6d07bc571dd78981f95e76fd36df63589d5e3a34
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-03-05 12:57:39 +0100

    hashcode and equals in productsImpl

--- app/src/main/java/unieuroop/model/product/ProductImpl.java
+++ app/src/main/java/unieuroop/model/product/ProductImpl.java
@@ -108,4 +108,64 @@ public final class ProductImpl implements Product {
             this.discountPercentage = Optional.of(discount);
         }
     }
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((category == null) ? 0 : category.hashCode());
+        result = prime * result + ((description == null) ? 0 : description.hashCode());
+        result = prime * result + ((discountPercentage == null) ? 0 : discountPercentage.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + productCode;
+        result = prime * result + Float.floatToIntBits(purchasePrice);
+        result = prime * result + Float.floatToIntBits(sellingPrice);
+        return result;
+    }
+    @Override
+    public boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ProductImpl other = (ProductImpl) obj;
+        if (category != other.category) {
+            return false;
+        }
+        if (description == null) {
+            if (other.description != null) {
+                return false;
+            }
+        } else if (!description.equals(other.description)) {
+            return false;
+        }
+        if (discountPercentage == null) {
+            if (other.discountPercentage != null) {
+                return false;
+            }
+        } else if (!discountPercentage.equals(other.discountPercentage)) {
+            return false;
+        }
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!name.equals(other.name)) {
+            return false;
+        }
+        if (productCode != other.productCode) {
+            return false;
+        }
+        if (Float.floatToIntBits(purchasePrice) != Float.floatToIntBits(other.purchasePrice)) {
+            return false;
+        }
+        if (Float.floatToIntBits(sellingPrice) != Float.floatToIntBits(other.sellingPrice)) {
+            return false;
+        }
+        return true;
+    }
 }
