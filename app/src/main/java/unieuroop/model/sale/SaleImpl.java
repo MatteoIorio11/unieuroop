62cfcf6abe2fffe9fe0156fd4a8ef3422733bb09
Matteo <matteo.iorio01@gmail.com>
2022-03-05 10:55:11 +0100

    creation and implementation of the method GetTotalPrice and get Date

--- app/src/main/java/unieuroop/model/sale/SaleImpl.java
+++ app/src/main/java/unieuroop/model/sale/SaleImpl.java
@@ -4,6 +4,7 @@ import unieuroop.model.product.Product;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 
@@ -12,11 +13,12 @@ public class SaleImpl {
 	private final Date date;
 	private final Map<Product, Integer> productsBuyed;
 	private final Optional<Client> client;
+	/*private final int id;ID created with the hashCode ???????????*/
 	
 	/**
 	 * Constructor of Sale, it requires :
 	 * @param dateSale : date of the sale
-	 * @param products : the map of product buyed, I take the copy of the map
+	 * @param products : the map of product buyed and their quantity. Product-Quantity
 	 * @param client   : this parameter can be Empty, the most important thing is the Sale not who buyed 
 	 */
 	public SaleImpl(final Date dateSale, final Map<Product, Integer> products, final Optional<Client> client) {
@@ -27,16 +29,28 @@ public class SaleImpl {
 	
 	/**
 	 * 
+	 * @return the Date of the sale
+	 */
+	public Date getDate() {
+		return this.date;
+	}
+	
+	/**
+	 * 
 	 * @return the copy of all products buyed
 	 */
-	public Set<Product> getProducts(){
+	public Set<Product> getProducts() {
 		return Set.copyOf(this.productsBuyed.keySet());
 	}
 	
-	public Float getTotalPrice() {
+	/**
+	 * 
+	 * @return the total price of the sale 
+	 */
+	public float getTotalPrice() {
 		return this.productsBuyed.entrySet().stream()
-				.map((e) -> e.getKey().getSellingPrice())
-				.reduce(0, (price1, price2) -> price1+price2);
+				.map((e) -> e.getKey().getSellingPrice() * e.getValue())
+				.reduce((price1, price2) -> price1 + price2).get();
 	}
 	
 	
