cb4af1e19366ede9d568bab4145ebad2e94f3b4d
Matteo <matteo.iorio01@gmail.com>
2022-03-06 10:48:27 +0100

    cration of the TEST : TestAnalytic

--- app/src/test/java/unieuroop/test/analytic/TestAnalytic.java
+++ app/src/test/java/unieuroop/test/analytic/TestAnalytic.java
@@ -1,5 +1,19 @@
 package unieuroop.test.analytic;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class TestAnalytic {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
 
 }
