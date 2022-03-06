ce4ba60bccb3402874dadbf338ff32e9cba3b569
Samuele Ferri <samuele1301@gmail.com>
2022-03-06 11:34:13 +0100

    fix error

--- app/src/main/java/unieuroop/model/person/AbstractPersonImpl.java
+++
@@ -1,9 +0,0 @@
-package unieuroop.model.person;
-
-import java.util.Date;
-
-public abstract class AbstractPersonImpl {
-	private String name;
-	private String surname;
-	private final String birthdayDate;
-}
