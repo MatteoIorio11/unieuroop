6b2f2f5ce21986d210444f70a708351fba8e1b5b
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-03-07 19:27:58 +0100

    created ShopImpl

--- app/src/main/java/unieuroop/model/shop/ShopImpl.java
+++ app/src/main/java/unieuroop/model/shop/ShopImpl.java
@@ -1,5 +1,115 @@
 package unieuroop.model.shop;
 
-public class ShopImpl {
+import java.util.HashSet;
+import java.util.NoSuchElementException;
+import java.util.Set;
 
+import unieuroop.model.department.Department;
+import unieuroop.model.person.Client;
+import unieuroop.model.person.Staff;
+import unieuroop.model.sale.Sale;
+import unieuroop.model.stock.Stock;
+import unieuroop.model.supplier.Supplier;
+
+public final class ShopImpl implements Shop {
+    private String name;
+    private final Set<Department> departments = new HashSet<>();
+    private final Set<Staff> staffs = new HashSet<>();
+    private final Set<Supplier> suppliers = new HashSet<>();
+    private final Set<Sale> sales = new HashSet<>();
+    private final Set<Client> registeredClients = new HashSet<>();
+    private final Stock stock = new StockImpl();
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public Set<Department> getDepartments() {
+        return Set.copyOf(this.departments);
+    }
+
+    @Override
+    public Set<Staff> getStaffs() {
+        return Set.copyOf(this.staffs);
+    }
+
+    @Override
+    public Set<Supplier> getSuppliers() {
+        return Set.copyOf(this.suppliers);
+    }
+
+    @Override
+    public Set<Sale> getSales() {
+        return Set.copyOf(this.sales);
+    }
+
+    @Override
+    public Set<Client> getRegisteredClients() {
+        return Set.copyOf(this.registeredClients);
+    }
+
+    @Override
+    public Stock getStock() {
+        return this.stock;
+    }
+
+    @Override
+    public void addDepartment(final Department department) {
+        this.departments.add(department);
+    }
+
+    @Override
+    public void addStaff(final Staff staff) {
+        this.staffs.add(staff);
+    }
+
+    @Override
+    public void addSupplier(final Supplier supplier) {
+        this.suppliers.add(supplier);
+    }
+
+    @Override
+    public void addSale(final Sale sale) {
+        this.sales.add(sale);
+    }
+
+    @Override
+    public void registerClient(final Client client) {
+        this.registeredClients.add(client);
+    }
+
+    @Override
+    public void removeDepartment(final Department department) {
+        this.departments.add(department);
+    }
+
+    @Override
+    public void removeStaff(final Staff staff) {
+        if (!this.staffs.remove(staff)) {
+            throw new NoSuchElementException();
+        }
+    }
+
+    @Override
+    public void removeSupplier(final Supplier supplier) {
+        if (!this.suppliers.remove(supplier)) {
+            throw new NoSuchElementException();
+        }
+    }
+
+    @Override
+    public void removeSale(final Sale sale) {
+        if (!this.sales.remove(sale)) {
+            throw new NoSuchElementException();
+        }
+    }
+
+    @Override
+    public void removeClient(final Client client) {
+        if (!this.registeredClients.remove(client)) {
+            throw new NoSuchElementException();
+        }
+    }
 }
