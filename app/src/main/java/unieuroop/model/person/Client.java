9eef2aa6d8f1adda25dcfc0b891d76f70a722c52
Samuele Ferri <samuele1301@gmail.com>
2022-03-07 09:05:57 +0100

    costructor for Client

--- app/src/main/java/unieuroop/model/person/Client.java
+++ app/src/main/java/unieuroop/model/person/Client.java
@@ -1,5 +1,14 @@
 package unieuroop.model.person;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
 public class Client extends AbstractPerson{
 
+	private final Optional<Integer> clientCode;
+	
+	public Client(final String name, final String surname, final LocalDate birthdayDate, final Optional<Integer> clientCode) {
+		super(name, surname, birthdayDate);
+		this.clientCode = clientCode;
+	}
 }
