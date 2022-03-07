981059b1433d24ea784e6aff7241691ea46ae8df
nicola_strada <nicolastrada5@gmail.com>
2022-03-07 22:50:45 +0100

    Stock Interface

--- app/src/main/java/unieuroop/model/stock/Stock.java
+++ app/src/main/java/unieuroop/model/stock/Stock.java
@@ -1,5 +1,18 @@
 package unieuroop.model.stock;
 
+import java.util.Set;
+import unieuroop.model.product.Product;
+
 public interface Stock {
+	
+	void addProduct(Product product, int amount);
+	
+	
+	Set<Product> getTotalStock();
+	
+	int getQuantityOfProduct(Product product);
+	
+	float getTotalPrice();
+	
 
 }
---
+++ app/src/main/java/unieuroop/model/stock/StockImpl.java
@@ -0,0 +1,5 @@
+package unieuroop.model.stock;
+
+public class StockImpl {
+
+}
