e218e6ed55a049b0f8fbc152a4b7a47d12799f69
nicola_strada <nicolastrada5@gmail.com>
2022-03-08 13:59:09 +0100

    Edit Stock

--- app/src/main/java/unieuroop/model/stock/Stock.java
+++ app/src/main/java/unieuroop/model/stock/Stock.java
@@ -1,18 +1,35 @@
 package unieuroop.model.stock;
 
 import java.util.Set;
+import java.util.Map;
 import unieuroop.model.product.Product;
 
 public interface Stock {
-	
-	void addProduct(Product product, int amount);
-	
-	
-	Set<Product> getTotalStock();
-	
-	int getQuantityOfProduct(Product product);
-	
-	float getTotalPrice();
-	
-
+    /**
+     * Add multiple products.
+     * @param products
+     */
+    void addProducts(Map<Product, Integer> products);
+    /**
+     * Return the entire stock of product.
+     * @return products
+     */
+    Map<Product, Integer> getTotalStock();
+    /**
+     * Return the quantity of a product.
+     * @param product
+     * @return quantityProducts
+     */
+    int getQuantityOfProduct(Product product);
+    /**
+     * Return the Map of the products taken from the stock, then they will be putted in the Department.
+     * @param productsTaken
+     * @return productsTaken
+     */
+    Map<Product, Integer> takeFromStock(Map<Product, Integer> productsTaken);
+    /**
+     * Permanent delete a set of Products.
+     * @param productsDelete
+     */
+    void deleteProduct(Set<Product> productsDelete);
 }
