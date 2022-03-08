24700507615643f6e950bb88950e922ff413bb3e
Matteo <matteo.iorio01@gmail.com>
2022-03-08 22:43:35 +0100

    writing getProducts inside the interface Department

--- app/src/main/java/unieuroop/model/department/Department.java
+++ app/src/main/java/unieuroop/model/department/Department.java
@@ -20,17 +20,29 @@ public interface Department {
      */
     void removeProduct(Product product, int quantity);
     /**
-     * 
+     * With this method we can add a new the Staff inside the Department.
+     * @param staff we will add in the Department
+     */
+    void addStaff(Staff staff);
+    /**
+     * Remove a specified Staff inside  the Department.
+     * @param staff we will remove from the Department
+     */
+    void removeStaff(Staff staff);
+    /**
+     * Get all the staff inside the Department, the Set can be empty if nobody work inside this Department
+     * this can happen if the Department is new.
      * @return the Set of all Staff inside the department
      */
     Set<Staff> getStaff();
     /**
-     * 
+     * Return all the products inside the Department  without their quantity.
      * @return the Set of all Products inside the Department
      */
     Set<Product> getAllProducts();
     /**
-     * 
+     * If we want more information about the Products inside the Department we can call this method that
+     * return a Map with the Product and it's quantity.
      * @return the Map of every product inside the Department with their quantity
      */
     Map<Product, Integer> getProducts();
