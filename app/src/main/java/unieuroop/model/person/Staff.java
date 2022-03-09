329bb9477f7a31712d36e7a44d73f3ccc7d4020e
Samuele Ferri <samuele1301@gmail.com>
2022-03-09 12:39:06 +0100

    add costructor in Staff

--- app/src/main/java/unieuroop/model/person/Staff.java
+++ app/src/main/java/unieuroop/model/person/Staff.java
@@ -1,5 +1,19 @@
 package unieuroop.model.person;
 
-public interface Staff {
+import java.time.LocalDate;
 
+public class Staff extends AbstractPerson {
+
+    private final Integer id;
+    private final String email;
+    private final String password;
+    private final LocalDate workTime;
+    
+    public Staff(final String name, final String surname, final LocalDate birthdayDate, final Integer id, final String email, final String password, final LocalDate workTime) {
+        super(name, surname, birthdayDate);
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.workTime = workTime;
+    }
 }
