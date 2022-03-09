1106a0fdadd6bdd7f2539995e65f39d93626959c
nicola_strada <nicolastrada5@gmail.com>
2022-03-09 23:09:37 +0100

    Create Method in Supplier

--- app/src/main/java/unieuroop/model/supplier/Supplier.java
+++ app/src/main/java/unieuroop/model/supplier/Supplier.java
@@ -1,5 +1,32 @@
 package unieuroop.model.supplier;
 
-public interface Supplier {
+import java.util.Map;
+import java.util.Set;
+import unieuroop.model.product.Product;
 
+public interface Supplier {
+    /**
+     * Return the name of the supplier.
+     * @return supplierName
+     */
+    String getName();
+    /**
+     * Return all the supplier products. 
+     * @return supplierProducts
+     */
+    Set<Product> getCatalog();
+    /**
+     * Return the price of the product by their quantities.
+     * @param product
+     * @param amount
+     * @return productPrice
+     */
+    float getPriceByProduct(Product product, int amount);
+    /**
+     * Sells the quantities product requested.
+     * @param product
+     * @param amount
+     * @return productSold
+     */
+    Map<Product, Integer> sellProduct(Product product, int amount);
 }
---
+++ app/src/main/java/unieuroop/model/supplier/SupplierImpl.java
@@ -0,0 +1,34 @@
+package unieuroop.model.supplier;
+
+import java.util.Map;
+import java.util.Set;
+
+import unieuroop.model.product.Product;
+
+public class SupplierImpl implements Supplier {
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Set<Product> getCatalog() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public float getPriceByProduct(Product product, int amount) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Map<Product, Integer> sellProduct(Product product, int amount) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
