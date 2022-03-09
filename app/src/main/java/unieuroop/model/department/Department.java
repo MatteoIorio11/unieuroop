ef2a73fbcaa6fc3f91a7133558379a2263f3c97b
nicola_strada <nicolastrada5@gmail.com>
2022-03-09 12:49:23 +0100

    Auto stash before merge of "develop" and "origin/develop"

--- app/src/main/java/unieuroop/model/department/Department.java
+++ app/src/main/java/unieuroop/model/department/Department.java
@@ -2,48 +2,21 @@ package unieuroop.model.department;
 
 import java.util.Map;
 import java.util.Set;
-
 import unieuroop.model.person.Staff;
 import unieuroop.model.product.Product;
-public interface Department {
 
-    /**
-     * We call this method if we want to add a new Product inside the Department.
-     * @param product that will be add in the Department
-     * @param quantity of the product that will be add inside Department
-     */
-    void addProduct(Product product, int quantity);
-    /**
-     * Remove a specified product with a specified quantity.
-     * @param product that we want to remove from department
-     * @param quantity of we will remove from Department
-     */
-    void removeProduct(Product product, int quantity);
-    /**
-     * With this method we can add a new the Staff inside the Department.
-     * @param staff we will add in the Department
-     */
-    void addStaff(Staff staff);
-    /**
-     * Remove a specified Staff inside  the Department.
-     * @param staff we will remove from the Department
-     */
-    void removeStaff(Staff staff);
-    /**
-     * Get all the staff inside the Department, the Set can be empty if nobody work inside this Department
-     * this can happen if the Department is new.
-     * @return the Set of all Staff inside the department
-     */
+public interface Department {
+    void addProducts(Map<Product, Integer> products);
+    
+    void addStaff(Staff newStaff);
+    
+    void removeStaff(Staff deleteStaff);
+    
+    String getDepartmentName();
+    
+    Map<Product, Integer> productsbyQuantity(); //Con Function?
+    
     Set<Staff> getStaff();
-    /**
-     * Return all the products inside the Department  without their quantity.
-     * @return the Set of all Products inside the Department
-     */
-    Set<Product> getAllProducts();
-    /**
-     * If we want more information about the Products inside the Department we can call this method that
-     * return a Map with the Product and it's quantity.
-     * @return the Map of every product inside the Department with their quantity
-     */
-    Map<Product, Integer> getProducts();
+    
+    Map<Product, Integer> getAllProducts();
 }
--- app/src/main/java/unieuroop/model/stock/StockImpl.java
+++ app/src/main/java/unieuroop/model/stock/StockImpl.java
@@ -1,5 +1,39 @@
 package unieuroop.model.stock;
 
-public class StockImpl {
+import unieuroop.model.product.Product;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+
+public class StockImpl implements Stock {
+    private final Map<Product, Integer> productsStocked = new HashMap<Product, Integer>();
+
+    @Override
+    public void addProducts(Map<Product, Integer> products) {
+        for (Product product : products.keySet()) {
+            this.productsStocked.merge(product, products.get(product), (existingQuantity, newQuantity) -> existingQuantity + newQuantity);
+        }
+    }
+
+    @Override
+    public Map<Product, Integer> getTotalStock() {
+        return this.productsStocked;
+    }
+
+    @Override
+    public int getQuantityOfProduct(Product product) {
+        return this.productsStocked.getOrDefault(product, 0);
+    }
+
+    @Override
+    public Map<Product, Integer> takeFromStock(Map<Product, Integer> productsTaken) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void deleteProduct(Set<Product> productsDelete) {
+        this.productsStocked.keySet().removeAll(productsDelete);
+    }
 
 }
