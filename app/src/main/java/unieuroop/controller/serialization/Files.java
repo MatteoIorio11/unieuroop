dc4de43a00beec68e3f9b5d292111f3be7450a5b
Matteo <matteo.iorio01@gmail.com>
2022-03-11 15:35:04 +0100

    Creation of enum Files and it's values

--- app/src/main/java/unieuroop/controller/serialization/Files.java
+++ app/src/main/java/unieuroop/controller/serialization/Files.java
@@ -1,5 +1,42 @@
 package unieuroop.controller.serialization;
 
 public enum Files {
-    
+    /**
+     * @param fileName Products.json
+     */
+    PRODUCTS("Products.json"),
+    /**
+     * @param fileName Products.json
+     */
+    STAFFS("Staffs.json"),
+    /**
+     * @param fileName Products.json
+     */
+    SALES("Sales.json"),
+    /**
+     * @param fileName Products.json
+     */
+    STOCK("Stock.json"),
+    /**
+     * @param fileName Products.json
+     */
+    SUPPLIERS("Suppliers.json"),
+    /**
+     * @param fileName Products.json
+     */
+    SHOP("Shop.json"),
+    /**
+     * @param fileName Products.json
+     */
+    DEPARTMENTS("Departments.json");
+
+    private final String fileName;
+    private static final String SEPARATOR = System.getProperty("file.separator");
+    Files(final String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getPath(){
+        return Files.SEPARATOR + this.fileName;
+    }
 }
