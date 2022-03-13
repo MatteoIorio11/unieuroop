9398a9ca486bea8083894e06cfdd7208345e7ca3
Matteo <matteo.iorio01@gmail.com>
2022-03-13 09:54:21 +0100

    Creation of emptys test that will be implemented by my group

---
+++ app/src/test/java/unieuroop/test/shop/TestShop.java
@@ -0,0 +1,19 @@
+package unieuroop.test.shop;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestShop {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
