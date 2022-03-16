c39a6f1827ea9006bbb1ee9d90dd269390094863
Samuele Ferri <samuele1301@gmail.com>
2022-03-16 12:50:24 +0100

    keep initialize testShop

--- app/src/test/java/unieuroop/test/shop/TestShop.java
+++ app/src/test/java/unieuroop/test/shop/TestShop.java
@@ -2,9 +2,11 @@ package unieuroop.test.shop;
 
 import static org.junit.Assert.assertEquals;
 
+import java.nio.MappedByteBuffer;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 import org.junit.Before;
@@ -13,29 +15,46 @@ import org.junit.Test;
 import unieuroop.model.department.Department;
 import unieuroop.model.department.DepartmentImpl;
 import unieuroop.model.person.Staff;
+import unieuroop.model.product.Category;
+import unieuroop.model.product.Product;
+import unieuroop.model.product.ProductImpl;
 import unieuroop.model.shop.Shop;
 import unieuroop.model.shop.ShopImpl;
+import unieuroop.model.supplier.Supplier;
+import unieuroop.model.supplier.SupplierImpl;
 
 public class TestShop {
 
+    private static final String APPLE_PRODUCT = "APPLE"; /*Brand of products*/
     private static final LocalDate TIME_NOW = LocalDate.now();
     private static final LocalTime TIME_START = LocalTime.now();
     private static final LocalTime TIME_FINISH = LocalTime.of(10, 10);
     private Department department; 
     private Shop shop01;
+    private final Supplier s1 = new SupplierImpl();
     private final Staff staff1 = new Staff("Nome1", "Cognome1", TestShop.TIME_NOW,
             0, "email1@gmail.com", "password1", TIME_START, TIME_FINISH);
     private final Staff staff2 = new Staff("Nome2", "Cognome2", TestShop.TIME_NOW,
             0, "email2@gmail.csom", "password2", TIME_START, TIME_FINISH);
+    private final Staff staff3 = new Staff("Nome3", "Cognome4", TestShop.TIME_NOW,
+            0, "email3@gmail.com", "password3", TIME_START, TIME_FINISH);
+    private final Staff staff4 = new Staff("Nome4", "Cognome4", TestShop.TIME_NOW,
+            0, "email4@gmail.csom", "password4", TIME_START, TIME_FINISH);
+    private final Product p1 = new ProductImpl(1, "iphone 13 pro", TestShop.APPLE_PRODUCT,  1200.00,  900.00, Optional.empty(), "best phone ever created", Category.SMARTPHONE, s1);
+    private final Product p2 = new ProductImpl(2, "applewatch", TestShop.APPLE_PRODUCT, 500.00,  200.00, Optional.empty(), "best watch ever created", Category.SMARTWATCH, s1);
+    private final Product p3 = new ProductImpl(3, "mac book pro 14 ", TestShop.APPLE_PRODUCT,  3000.00, 2000.00, Optional.empty(), "best mac book ever created", Category.PC, s1);
+    private final Product p4 = new ProductImpl(4, "mac book pro 16", TestShop.APPLE_PRODUCT,  6000.00,  3000.00, Optional.empty(), "best mac book ever created", Category.PC, s1);
 
     @Before
     public void setUp() throws Exception {
         this.shop01 = new ShopImpl("shop01");
-        this.department = new DepartmentImpl("department1", Set.of(staff1, staff2), Map.of());
+        this.department = new DepartmentImpl("department1", Set.of(staff1, staff2), Map.of(p1, 10, p4, 3));
+        this.shop01.addDepartment(department);
+        this.department = new DepartmentImpl("department2", Set.of(staff3, staff4), Map.of(p2, 1, p3, 2));
     }
 
     @Test
-    public void test() {
+    public void testMergeDepartments() {
         
     }
 
