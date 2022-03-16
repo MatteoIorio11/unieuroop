9ab60511052b3e6f07632bdee66bf450f17afb19
nicola_strada <nicolastrada5@gmail.com>
2022-03-16 13:19:25 +0100

    Edit Department methods

--- app/src/main/java/unieuroop/model/department/DepartmentImpl.java
+++ app/src/main/java/unieuroop/model/department/DepartmentImpl.java
@@ -10,22 +10,30 @@ import unieuroop.model.person.Staff;
 import unieuroop.model.product.Product;
 
 public final class DepartmentImpl implements Department {
+
     private final String name;
     private final Set<Staff> staff;
     private final Map<Product, Integer> products;
 
-    public DepartmentImpl(final String nameDepartment, final Set<Staff> staff, 
-            final Map<Product, Integer> products) {
+    public DepartmentImpl(final String nameDepartment, final Set<Staff> staff, final Map<Product, Integer> products) {
         this.name = nameDepartment;
-        this.staff = new HashSet<>(Set.copyOf(staff));
-        this.products = new HashMap<>(Map.copyOf(products));
+        this.staff = new HashSet<>(staff);
+        this.products = new HashMap<>(products);
     }
 
+    /**
+     * 
+     */
     @Override
     public void addProducts(final Map<Product, Integer> products) {
-        
+        for (final Product product : products.keySet()) {
+            this.products.merge(product, products.get(product), (quantityPresent, newQuantity) -> quantityPresent + newQuantity);
+        }
     }
 
+    /**
+     * 
+     */
     @Override
     public void addStaff(final Staff newStaff) {
         if (!this.staff.contains(newStaff)) {
@@ -35,27 +43,46 @@ public final class DepartmentImpl implements Department {
         }
     }
 
+    /**
+     * 
+     */
     @Override
     public void removeStaff(final Staff deleteStaff) {
-        
+        if (this.staff.contains(deleteStaff)) {
+            this.staff.remove(deleteStaff);
+        } else {
+            throw new IllegalArgumentException("The staff : " + deleteStaff.toString() + " not exist.");
+        }
     }
 
+    /**
+     * 
+     */
     @Override
     public String getDepartmentName() {
         return this.name;
     }
 
+    /**
+     * 
+     */
     @Override
     public Map<Product, Integer> productsByQuantity(final Predicate<Integer> quantity) {
         // TODO Auto-generated method stub
         return null;
     }
 
+    /**
+     * 
+     */
     @Override
     public Set<Staff> getStaff() {
         return Set.copyOf(this.staff);
     }
 
+    /**
+     * 
+     */
     @Override
     public Map<Product, Integer> getAllProducts() {
         return Map.copyOf(this.products);
