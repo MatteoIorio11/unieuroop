5a746332b39cd15008973dac4ccfe816c625c0a9
Matteo <matteo.iorio01@gmail.com>
2022-03-20 13:00:29 +0100

    Implementation of areaChart

--- app/src/main/java/unieuroop/model/sale/Sale.java
+++ app/src/main/java/unieuroop/model/sale/Sale.java
@@ -8,37 +8,36 @@ import unieuroop.model.person.Client;
 import unieuroop.model.product.Product;
 
 public interface Sale {
-	
-    /**
-	* 
-	* @return the specific Date of the sale
-	*/
-    LocalDate getDate();
-	
-	/**
-	* 
-	* @return the copy of all products buyed, stored in a Set
-	*/
-	Set<Product> getProducts();
-	
-	/**
-	* 
-	* @return the total price of the sale 
-	*/
-	double getTotalSpent();
-	
-	/**
-	 * 
-	 * @param product
-	 * @return the total quantity of the specified product, if the product is not contained, 
-	 * I return 0
-	 */
-	int getQuantityOf(Product product);
-	
-	/**
-	 * 
-	 * @return the client of this specific Sale, is optional
-	 */
-	Optional<Client> getClient();
 
+    /**
+    * 
+    * @return the specific Date of the sale
+    */
+    LocalDate getDate();
+
+    /**
+    * 
+    * @return the copy of all products buyed, stored in a Set
+    */
+    Set<Product> getProducts();
+
+    /**
+    * 
+    * @return the total price of the sale 
+    */
+    double getTotalSpent();
+
+    /**
+     * 
+     * @param product
+     * @return the total quantity of the specified product, if the product is not contained, 
+     * I return 0
+     */
+    int getQuantityOf(Product product);
+
+    /**
+     * 
+     * @return the client of this specific Sale, is optional
+     */
+    Optional<Client> getClient();
 }
