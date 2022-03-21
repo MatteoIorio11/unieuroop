f0617a2221424ac88e871311c0f6596ad6094dac
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-03-21 13:25:53 +0100

    fixing loginController

--- app/src/main/java/unieuroop/controller/login/LoginControllerImpl.java
+++ app/src/main/java/unieuroop/controller/login/LoginControllerImpl.java
@@ -1,5 +1,18 @@
 package unieuroop.controller.login;
 
-public class LoginControllerImpl {
+import java.util.HashMap;
+import java.util.Map;
 
+import unieuroop.model.person.Staff;
+import unieuroop.model.shop.Shop;
+
+public class LoginControllerImpl {
+    private final Shop shop;
+    public LoginControllerImpl(Shop shop) {
+        this.shop = shop;
+    }
+    public boolean CheckPassword(String email, String password) {
+        return false;
+        //return this.shop.getStaffs().stream().anyMatch(s -> s.getEmail() == email && s.getPassword() == password.hashCode());
+    }
 }
