b224b70cba45c6d0e781e09b1b3091fae6f5a5fc
Matteo <matteo.iorio01@gmail.com>
2022-03-31 23:13:37 +0200

    Init of TestStock

---
+++ app/src/test/java/unieuroop/test/stock/TestStock.java
@@ -0,0 +1,14 @@
+package unieuroop.test.stock;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestStock {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
