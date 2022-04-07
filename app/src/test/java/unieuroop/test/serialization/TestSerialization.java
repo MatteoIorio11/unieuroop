ce1d1363a61c4122ac520de736cbfaee1a3ff19e
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-04-07 14:03:33 +0200

    stock serialization test

--- app/src/test/java/unieuroop/test/serialization/TestSerialization.java
+++ app/src/test/java/unieuroop/test/serialization/TestSerialization.java
@@ -1,6 +1,6 @@
 package unieuroop.test.serialization;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.io.IOException;
 import java.time.DayOfWeek;
@@ -21,6 +21,8 @@ import unieuroop.model.person.Staff;
 import unieuroop.model.product.Category;
 import unieuroop.model.product.Product;
 import unieuroop.model.product.ProductImpl;
+import unieuroop.model.stock.Stock;
+import unieuroop.model.stock.StockImpl;
 
 public class TestSerialization {
 
@@ -30,7 +32,7 @@ public class TestSerialization {
 
     @Test
     public void testStaff() throws IOException, ClassNotFoundException {
-        Product p = new ProductImpl(1, "prova","provaBrand",12.4, 15.0, "bla bla bla bla bla", Category.PC);
+        Product p = new ProductImpl(1, "prova", "provaBrand", 12.4, 15.0, "bla bla bla bla bla", Category.PC);
         final Staff s = new Staff("nome", "cognome", LocalDate.now(), 1, "ffff", 111, Map.of(DayOfWeek.of(1), new Pair<LocalTime, LocalTime>(LocalTime.now(), LocalTime.now())));
         final Staff s1 = new Staff("nome1", "cognome1", LocalDate.now(), 1, "AAAA", 222, Map.of(DayOfWeek.of(1), new Pair<LocalTime, LocalTime>(LocalTime.now(), LocalTime.now())));
         final Set<Staff> staff = new HashSet<>();
@@ -39,6 +41,19 @@ public class TestSerialization {
         Serialization.<Set<Staff>>serialize(Files.STAFFS.getPath(), staff);
         final Set<Staff> deserializedStaff = Serialization.<Set<Staff>>deserialize(Files.STAFFS.getPath(), new TypeReference<Set<Staff>>() { });
         assertEquals(staff, deserializedStaff);
+    }
+    @Test
+    public void testStock() throws IOException, ClassNotFoundException {
+        final Product p = new ProductImpl(1, "prova", "provaBrand", 12.4, 15.0, "bla bla bla bla bla", Category.PC);
+        final Product p1 = new ProductImpl(2, "prova1", "provaBrand1", 12.4, 15.0, "bla bla bla bla bla", Category.HOME);
+        final Product p2 = new ProductImpl(3, "prova2", "provaBrand2", 12.4, 15.0, "bla bla bla bla bla", Category.TABLET);
+        final Product p3 = new ProductImpl(4, "prova3", "provaBrand1", 12.4, 15.0, "bla bla bla bla bla", Category.PC);
+        final Product p4 = new ProductImpl(5, "prova4", "provaBrand2", 12.4, 15.0, "bla bla bla bla bla", Category.PC);
+        final Stock stock = new StockImpl();
+        stock.addProducts(Map.of(p, 10, p1, 10, p2, 2, p3, 32, p4, 32));
+        Serialization.<Stock>serialize(Files.STOCK.getPath(), stock);
+        final Stock deserializedStock = Serialization.<Stock>deserialize(Files.STOCK.getPath(), new TypeReference<Stock>() { });
+        assertEquals(stock, deserializedStock);
     }
 
 }
