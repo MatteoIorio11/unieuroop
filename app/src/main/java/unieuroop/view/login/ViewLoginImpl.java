c4f3964e1571a625bc8bb1fc539c536be265e5eb
Matteo <matteo.iorio01@gmail.com>
2022-04-08 16:08:29 +0200

    Removing stage from ViewLogin

--- app/src/main/java/unieuroop/view/login/ViewLoginImpl.java
+++ app/src/main/java/unieuroop/view/login/ViewLoginImpl.java
@@ -20,7 +20,7 @@ import unieuroop.controller.serialization.Pages;
 import unieuroop.controller.shop.ControllerShopImpl;
 import unieuroop.view.menu.ViewMainMenu;
 
-public class ViewLoginImpl implements Initializable {
+public final class ViewLoginImpl implements Initializable {
     private final ControllerLoginImpl controller;
     private final Stage primaryStage;
     @FXML 
@@ -40,10 +40,10 @@ public class ViewLoginImpl implements Initializable {
         }
     }
     @FXML
-    private void btnLoginHandler(final ActionEvent event) throws IOException {
+    public void btnLoginHandler(final ActionEvent event) throws IOException {
         if (this.controller.checkPassword(this.email.getText(), this.password.getText())) {
             final var loader = new FXMLLoader(getClass().getResource("/pages/MainMenu.fxml"));
-            loader.setController(new ViewMainMenu(primaryStage, new ControllerShopImpl(this.controller.getShop())));
+            loader.setController(new ViewMainMenu(new ControllerShopImpl(this.controller.getShop())));
             final Parent root = loader.load();
           final Scene scene = new Scene(root, 1000, 600);
           primaryStage.setTitle("unieurOOP");
@@ -52,7 +52,7 @@ public class ViewLoginImpl implements Initializable {
           primaryStage.setMinWidth(1000);
           primaryStage.show();
         } else {
-            final Alert alert = new Alert(AlertType.INFORMATION);
+            final Alert alert = new Alert(AlertType.ERROR);
             alert.setContentText("Wrong Password");
             alert.showAndWait();
         }
