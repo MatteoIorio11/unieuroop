51e8e09f1f3571ec513743f310935c93c7768f97
nicola_strada <nicolastrada5@gmail.com>
2022-04-08 21:26:30 +0200

    Add new StockLabel for buy products and implementation of new method in StockView and StockBuyProducts

---
+++ app/src/main/java/unieuroop/view/stock/ViewStockLabelBuyProducts.java
@@ -0,0 +1,16 @@
+package unieuroop.view.stock;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class ViewStockLabelBuyProducts implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
