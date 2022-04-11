cd4f41507bdff29c4d0843ee0a37c39bc949c64d
nicola_strada <nicolastrada5@gmail.com>
2022-04-11 18:53:05 +0200

    Implementation of Methods in StockController and ViewStockSearchFilter, Edit ViewStock

--- app/src/main/java/unieuroop/view/stock/ViewStockSetFilters.java
+++ app/src/main/java/unieuroop/view/stock/ViewStockSetFilters.java
@@ -1,5 +1,53 @@
 package unieuroop.view.stock;
 
-public class ViewStockSetFilters {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import unieuroop.controller.stock.ControllerStockImpl;
+import unieuroop.model.product.Category;
+
+public class ViewStockSetFilters implements Initializable {
+
+    @FXML
+    private Label lblMinAmount;
+    @FXML
+    private Slider sliderMinAmount;
+    @FXML
+    private Label lblMaxAmount;
+    @FXML
+    private Slider sliderMaxAmount;
+    @FXML
+    private ComboBox<Category> cmboxCategories;
+    @FXML
+    private Button btnConfirmFilter;
+
+    private ViewStock viewStock;
+    private ControllerStockImpl controllerStock;
+
+    public ViewStockSetFilters(final ViewStock viewStock, final ControllerStockImpl controller) {
+        this.viewStock = viewStock;
+        this.controllerStock = controller;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+    }
+
+    @FXML
+    public void btnConfirmFilterHandler() {
+        
+    }
+
+    @FXML
+    public void cmboxCategoriesHandler() {
+        
+    }
 
 }
