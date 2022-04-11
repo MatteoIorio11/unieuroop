e688e5c579adff6af157b143433e6912fd8b25d6
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-04-11 14:24:58 +0200

    adding documentation in controllerDashboard

--- app/src/main/java/unieuroop/controller/dashboard/ControllerDashboard.java
+++ app/src/main/java/unieuroop/controller/dashboard/ControllerDashboard.java
@@ -2,20 +2,38 @@ package unieuroop.controller.dashboard;
 
 
 import java.util.List;
-import java.util.Set;
-
 import unieuroop.model.sale.Sale;
 
 public interface ControllerDashboard {
 
+    /**
+     * 
+     * @return the ammount of staff of the shop.
+     */
     int getStaff();
 
+    /**
+     * 
+     * @return the ammount of Clients of the shop.
+     */
     int getClients();
 
+    /**
+     * 
+     * @return the ammount of Suppliers of the shop.
+     */
     int getSuppliers();
 
+    /**
+     * 
+     * @return the ammount of Departments of the shop.
+     */
     int getDepartments();
 
+    /**
+     * 
+     * @return an ordered list of sales by their dates.
+     */
     List<Sale> getSales();
 
 }
