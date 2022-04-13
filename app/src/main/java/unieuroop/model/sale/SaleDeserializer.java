2cc6a3edb825dfd5b0c8138b8e8a0b744ff374af
Matteo <matteo.iorio01@gmail.com>
2022-04-13 22:47:48 +0200

    refactory in SaleDeserializer

--- app/src/main/java/unieuroop/model/sale/SaleDeserializer.java
+++ app/src/main/java/unieuroop/model/sale/SaleDeserializer.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -24,22 +24,23 @@ public final class SaleDeserializer extends JsonDeserializer<Sale> {
     @Override
     public Sale deserialize(final JsonParser p, final DeserializationContext ctxt) throws IOException, JsonProcessingException {
         final JsonNode node = p.getCodec().readTree(p);
-        final var prod = new ArrayList<Product>();
-        final var qta = new ArrayList<Integer>();
+        final var product = new ArrayList<Product>();
+        final var quantity = new ArrayList<Integer>();
         final ObjectMapper mapper = ObjectMapperFactory.getMapper();
         final LocalDate date = mapper.treeToValue(node.get("date"), LocalDate.class);
-        for (final var el : node.get("products")) {
-            prod.add(mapper.treeToValue(el, ProductImpl.class));
+        for (final var element : node.get("products")) {
+            product.add(mapper.treeToValue(element, ProductImpl.class));
         }
-        for (final var el : node.get("quantity")) {
-            qta.add(el.asInt());
+        for (final var element : node.get("quantity")) {
+            quantity.add(element.asInt());
         }
-        final var m = new HashMap<Product, Integer>();
-        for (int i = 0; i < prod.size(); i++) {
-            m.put(prod.get(i), qta.get(i));
-        }
+        final var map = new HashMap<Product, Integer>();
+        IntStream.iterate(0, (i) -> i + 1).limit(product.size()).peek((i) -> map.put(product.get(i), quantity.get(i)));
+//        for (int i = 0; i < product.size(); i++) {
+//            map.put(product.get(i), quantity.get(i));
+//        }
         final var client = Optional.ofNullable(mapper.treeToValue(node.get("client"), Client.class));
-        return new SaleImpl(date, m, client);
+        return new SaleImpl(date, map, client);
     }
 
 }
--- app/src/main/java/unieuroop/model/sale/SaleSerializer.java
+++ app/src/main/java/unieuroop/model/sale/SaleSerializer.java
@@ -13,23 +13,23 @@ import unieuroop.controller.serialization.ObjectMapperFactory;
 public final class SaleSerializer extends JsonSerializer<Sale> {
 
     @Override
-    public void serialize(final Sale value, final JsonGenerator gen, final SerializerProvider serializers)
+    public void serialize(final Sale value, final JsonGenerator generator, final SerializerProvider serializers)
             throws IOException, JsonProcessingException {
-        gen.writeStartObject();
+        generator.writeStartObject();
         final ObjectMapper mapper = ObjectMapperFactory.getMapper();
-        gen.writeFieldName("date");
-        gen.writeRawValue(mapper.writeValueAsString(value.getDate()));
-        gen.writeArrayFieldStart("products");
+        generator.writeFieldName("date");
+        generator.writeRawValue(mapper.writeValueAsString(value.getDate()));
+        generator.writeArrayFieldStart("products");
         for (final var el : value.getProducts()) {
-            gen.writeObject(el);
+            generator.writeObject(el);
         }
-        gen.writeEndArray();
-        gen.writeArrayFieldStart("quantity");
+        generator.writeEndArray();
+        generator.writeArrayFieldStart("quantity");
         for (final var el : value.getProducts()) {
-            gen.writeObject(value.getQuantityOf(el));
+            generator.writeObject(value.getQuantityOf(el));
         }
-        gen.writeEndArray();
-        gen.writeObjectField("client", value.getClient());
-        gen.writeEndObject();
+        generator.writeEndArray();
+        generator.writeObjectField("client", value.getClient());
+        generator.writeEndObject();
     }
 }
