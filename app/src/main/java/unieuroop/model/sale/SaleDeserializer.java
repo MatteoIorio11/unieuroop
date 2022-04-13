e86335132f0881248f1bb71236b55e5112aba1e9
Matteo <matteo.iorio01@gmail.com>
2022-04-13 23:27:32 +0200

    Deletion of warnigns inside SaleDeserializer

--- app/src/main/java/unieuroop/model/sale/SaleDeserializer.java
+++ app/src/main/java/unieuroop/model/sale/SaleDeserializer.java
@@ -5,7 +5,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Optional;
-import java.util.stream.IntStream;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
