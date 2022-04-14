25ee74b30a849b512bce2de4d3d1c343e4c9555d
Samuele Ferri <samuele1301@gmail.com>
2022-04-14 12:16:27 +0200

    changed staffView and starded the implementations of ViewStaffImpl and ControllerStaffImpl

--- app/src/main/java/unieuroop/view/staff/ViewStaffImpl.java
+++ app/src/main/java/unieuroop/view/staff/ViewStaffImpl.java
@@ -1,11 +1,49 @@
 package unieuroop.view.staff;
 
+import java.awt.Button;
+import java.awt.Checkbox;
 import java.net.URL;
+import java.time.DayOfWeek;
 import java.util.ResourceBundle;
 
-import javafx.fxml.Initializable;
+import com.google.errorprone.annotations.FormatMethod;
 
-public class ViewStaffImpl implements Initializable{
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import unieuroop.model.person.Staff;
+
+public final class ViewStaffImpl implements Initializable {
+
+    @FXML
+    private ListView<Staff> listStaffs;
+    @FXML 
+    private TextField tbxName;
+    @FXML 
+    private TextField tbxSurname;
+    @FXML 
+    private DatePicker dtBirthday;
+    @FXML
+    private TextField tbxEmail;
+    @FXML
+    private TextField tbxPassword;
+    @FXML
+    private ComboBox<DayOfWeek> cbxDayOfWeek;
+    @FXML
+    private Checkbox chxNotWorked;
+    @FXML
+    private TextField tbxStartTime;
+    @FXML
+    private TextField tbxEndTime;
+    @FXML
+    private Button btnAddStaff;
+    @FXML
+    private Button btnEditStaff;
+    @FXML
+    private Button btnDeleteStaff;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
---
+++ app/src/main/java/unieuropp/controller/staff/ControllerStaffImpl.java
@@ -0,0 +1,13 @@
+package unieuropp.controller.staff;
+
+import unieuroop.model.shop.Shop;
+
+public class ControllerStaffImpl {
+
+    private final Shop shop;
+
+    public ControllerStaffImpl(final Shop shop) {
+        this.shop = shop;
+    }
+
+}
