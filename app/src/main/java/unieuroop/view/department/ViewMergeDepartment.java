0c924eb6af0b787bf120003c741cf9a8d23deee0
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-04-16 16:50:29 +0200

    adding label to merge departments

--- app/src/main/java/unieuroop/view/department/ViewMergeDepartment.java
+++ app/src/main/java/unieuroop/view/department/ViewMergeDepartment.java
@@ -2,9 +2,12 @@ package unieuroop.view.department;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -15,16 +18,19 @@ import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import unieuroop.controller.department.ControllerDepartmentImpl;
+import unieuroop.controller.serialization.Pages;
 import unieuroop.model.department.Department;
+import unieuroop.view.loader.Loader;
 
 public final class ViewMergeDepartment implements Initializable {
-    @FXML private ListView<Department> listDepartments;
+    @FXML private ListView<Pane> listDepartments;
     @FXML private TextField textName;
     @FXML private Button buttonMergeDepartment;
 
+    private final Map<Pane, ViewMergeLabelImpl> paneLabel = new HashMap<>();
     private final ControllerDepartmentImpl controllerDepartment;
     private final Set<Department> selectedDepartments = new HashSet<>();
     public ViewMergeDepartment(final ControllerDepartmentImpl controllerDepartment) {
@@ -33,12 +39,26 @@ public final class ViewMergeDepartment implements Initializable {
 
     @Override
     public void initialize(final URL location, final ResourceBundle resources) {
-        this.listDepartments.getItems().addAll(this.controllerDepartment.getDepartments());
+        this.controllerDepartment.getDepartments().stream().forEach(d -> {
+            try {
+                final var view = new ViewMergeLabelImpl(d);
+                final var pane = Loader.<ViewMergeLabelImpl>loadPane(Pages.LABEL_MERGE_DEPARTMENT.getPath(), view);
+                this.paneLabel.put(pane,  view);
+                this.listDepartments.getItems().add(pane);
+            } catch (IOException e) {
+                final Alert alert = new Alert(AlertType.ERROR);
+                alert.setContentText(e.getMessage());
+                alert.showAndWait();
+            }
+        });
     }
 
-    @FXML
-    public void listSelectDepartmentHandler(final MouseEvent event) {
-        this.selectedDepartments.add(this.listDepartments.getSelectionModel().getSelectedItem());
+    private Set<Department> getSelectedDepartments() {
+        return this.listDepartments.getItems().stream()
+        .map(d -> this.paneLabel.get(d))
+        .filter(d -> d.isSelected())
+        .map(l -> l.getDepartment())
+        .collect(Collectors.toSet());
     }
 
     @FXML
@@ -46,7 +66,7 @@ public final class ViewMergeDepartment implements Initializable {
         try {
             final Alert alert = new Alert(AlertType.INFORMATION);
             alert.setContentText("Are you sure to merge the selected department" + "\n" 
-            + this.selectedDepartments);
+            + this.getSelectedDepartments());
             alert.showAndWait();
             if (alert.getResult() == ButtonType.OK) {
                 this.controllerDepartment.mergeDepartments(this.selectedDepartments, this.textName.getText());
