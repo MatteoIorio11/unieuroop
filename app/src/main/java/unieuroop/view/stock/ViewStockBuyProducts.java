3c3f133bd7686d1985be332192b49eef7c0433cf
nicola_strada <nicolastrada5@gmail.com>
2022-04-16 18:01:30 +0200

    Fix some errors in Stock

--- app/src/main/java/unieuroop/view/stock/ViewStockBuyProducts.java
+++ app/src/main/java/unieuroop/view/stock/ViewStockBuyProducts.java
@@ -77,7 +77,7 @@ public class ViewStockBuyProducts implements Initializable {
         this.listSoldProducts.getItems().clear();
         for (final Map.Entry<Product, Double> entryProductSold : productsSold.entrySet()) {
             try {
-                final var pane = Loader.<ViewStockLabelBuyProducts>loadPane(Pages.LABEL_PRODUCT.getPath(), new ViewStockLabelBuyProducts(entryProductSold, this, this.controllerStock));
+                final var pane = Loader.<ViewStockLabelBuyProducts>loadPane(Pages.STOCK_LABEL_FOR_STOCKBUYPRODUCTS.getPath(), new ViewStockLabelBuyProducts(entryProductSold, this, this.controllerStock));
                 this.listSoldProducts.getItems().add(pane);
             } catch (IOException e) {
                 final Alert alert = new Alert(AlertType.ERROR);
