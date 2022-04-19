3b6e1305373ba8d2a442951467ebd6c3754af7a7
Samuele Ferri <samuele1301@gmail.com>
2022-04-19 16:15:28 +0200

    fix warnings and fix commit

--- app/src/main/java/unieuroop/view/categoryanalytic/ViewCategoriesSold.java
+++ app/src/main/java/unieuroop/view/categoryanalytic/ViewCategoriesSold.java
@@ -7,24 +7,12 @@ import javafx.scene.input.MouseEvent;
 
 public interface ViewCategoriesSold {
 
-    @FXML private BarChart<String, Integer> barCategories;
-    @FXML private BarChart<String, Integer> barProductsSold;
-    @FXML private ComboBox<Category> comboCategories;
-    @FXML private final CategoryAxis xAxis = new CategoryAxis();
-    @FXML private final NumberAxis yAxis = new NumberAxis();
-    @FXML private ListView<String> listLegend;
-    @FXML private ListView<String> listSelectedCategories;
-    private final Set<Category> selectedCategories = new HashSet<>();
-    private final ControllerAnalytic controller;
-
-    public ViewCategoriesSold(final ControllerAnalytic controller) {
-        this.controller = controller;
-    }
-
-    @Override
-    public void initialize(final URL location, final ResourceBundle resources) {
-        this.barCategories.setLegendVisible(false);
-        this.barProductsSold.setLegendVisible(false);
+    /**
+     * initialize new ViewCategorisSold.
+     * @param location
+     * @param resource
+     */
+    void initialize(URL location, ResourceBundle resource);
 
     /**
      * handler for clearing view.
@@ -44,46 +32,4 @@ public interface ViewCategoriesSold {
      */
     void listSelectValueHandler(MouseEvent event);
 
-    @FXML
-    public void comboSelectCategoryHandler(final ActionEvent event) {
-        if (!this.selectedCategories.contains(this.comboCategories.getValue())) {
-            this.selectedCategories.add(this.comboCategories.getValue());
-            this.displayChart();
-        }
-    }
-
-    @FXML
-    public void listSelectValueHandler(final MouseEvent event) {
-        final var string = this.listLegend.getSelectionModel().getSelectedItem();
-        if (string != null) {
-            final var code = string.split(":")[1].split("\n")[0].split(" ")[1];
-            final var dataInput = this.barProductsSold.getData().stream().flatMap((s) -> s.getData().stream().filter((data) -> data.getXValue().equals(code))).findAny();
-            dataInput.get().getNode().setStyle("-fx-bar-fill: blue;");
-        }
-    }
-
-    /**
-     * 
-     */
-    private void displayChart() {
-        this.barProductsSold.getData().clear();
-        final XYChart.Series<String, Integer> productSerie = new XYChart.Series<>();
-        productSerie.setName("Products");
-        final var categoriesSold =  this.controller.getProductsSoldByCategory(selectedCategories);
-        this.barProductsSold.getData().clear();
-        categoriesSold.entrySet().forEach((entry) ->
-            productSerie.getData().add(new XYChart.Data<String, Integer>(String.valueOf(entry.getKey().getProductCode()), entry.getValue())));
-        this.listLegend.getItems().clear();
-        this.listSelectedCategories.getItems().clear();
-        final var out = categoriesSold.entrySet().stream().map((entry) -> "Code : " + entry.getKey().getProductCode() + "\n" 
-                + "Name : " + entry.getKey().getName() + "\n"
-                + "Category : " + entry.getKey().getCategory() + "\n"
-                + "Quantity : " + entry.getValue())
-                   .collect(Collectors.toList());
-        this.listLegend.getItems().addAll(out);
-
-        this.listSelectedCategories.getItems().addAll(this.selectedCategories.stream()
-                .map((cat) -> cat.toString()).collect(Collectors.toList()));
-        this.barProductsSold.getData().add(productSerie);
-    }
 }
