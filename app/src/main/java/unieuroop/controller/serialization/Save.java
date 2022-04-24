4ce3b16973fae4e1fbc1cbb5188aa870a5b49d4b
Fabio Vincenzi <fabio.vincenzi2@studio.unibo.it>
2022-04-24 11:55:13 +0200

    adding documentation to class save

--- app/src/main/java/unieuroop/controller/serialization/Save.java
+++ app/src/main/java/unieuroop/controller/serialization/Save.java
@@ -1,7 +1,6 @@
 package unieuroop.controller.serialization;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,6 +16,10 @@ public final class Save {
 
     private Save() {
     }
+    /**
+     * It creates the directory and copy the json files inside it from the resources of the program if this folder doesn't exists already.
+     * @throws IOException
+     */
     public static void createDirectory() throws IOException {
         final File directory = new File(HOME_DIRECTORY + DATA_DIRECTORY);
         if (!Save.checkExistingDirectory(directory)) {
@@ -38,11 +41,21 @@ public final class Save {
             }
         }
     }
+    /**
+     * It says if the passed directory contains all the files that we need.
+     * @param directory
+     * @return
+     */
     private static boolean checkDirectoryContainsFiles(final File directory) {
         final List<String> files = List.of(directory.listFiles()).stream()
                 .map(f -> f.getName()).collect(Collectors.toList());
         return List.of(Files.values()).stream().allMatch(f -> files.contains(f.getName()));
     }
+    /**
+     * It says if the passed directory exist and it's a directory.
+     * @param directory
+     * @return
+     */
     private static boolean checkExistingDirectory(final File directory) {
         return directory.exists() && directory.isDirectory();
     }
